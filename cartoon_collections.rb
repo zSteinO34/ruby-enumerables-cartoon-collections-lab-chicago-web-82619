35d6950f0e1ff54e19b4988d4c25cdae29dbfe8d
Zac Steinberg <zstein1234@gmail.com>
2019-08-16 00:33:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,10 @@ def long_planeteer_calls(arr)
   return FALSE
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
+  arr.map { |str|
+    if str == "cheddar" || 
+  }
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
