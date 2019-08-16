eff944d953168b193b1c1717e71dbddc975302dc
Zac Steinberg <zstein1234@gmail.com>
2019-08-16 00:24:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,8 @@ def summon_captain_planet(arr)
   return new
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  
 end
 
 def find_the_cheese# code an argument here
